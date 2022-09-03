7ac17c4d65cad83e7765680cdf268411b0986072
cristianh6 <cristian.casalaspro@gmail.com>
2022-09-03 19:18:22 -0300

    Tp Formularios terminado

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ let port = 3000;
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(port,() => console.log(` server on port ${port}\n http://localhost:${port}`)
-);
-
-
-
-
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });
+
+app.get("/register",(req,res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+
+app.get("/login",(req,res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
+
+
+app.listen(port,() => console.log(` server on port ${port}\n http://localhost:${port}`)
+);
